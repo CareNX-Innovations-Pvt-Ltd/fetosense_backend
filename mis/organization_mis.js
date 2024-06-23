d610aabc03c0cacbd3d579f877e64961d7900e0d
Harshad NArkhede <harshad@carenx.com>
2024-06-23 10:57:42 +0530

    fix 3

--- mis/organization_mis.js
+++ mis/organization_mis.js
@@ -68,52 +68,52 @@ exports.getData = (req, res, next) => {
         console.log("documents.length", documents.length);
         var allDocuments = JSON.parse(JSON.stringify(documents));
         console.log("testAggrQuery", testAggrQuery);
-        Tests.aggregate(testAggrQuery, {
+        // Tests.aggregate(testAggrQuery, {
+        //     maxTimeMS: 240000
+        // }, function (err, tdocs) {
+        //     if (err) {
+        //         console.error("Aggregation error:", err);
+        //         return;
+        //     }
+        //     console.log("tdocs.length", tdocs.length);
+        //     for (tdoc of tdocs) {
+        //         testsCounts[tdoc._id.organizationId] = tdoc.totalTests;
+        //     }
+        console.log("mothersAggrQuery", mothersAggrQuery);
+        Users.aggregate(mothersAggrQuery, {
             maxTimeMS: 240000
-        }, function (err, tdocs) {
-            if (err) {
-                console.error("Aggregation error:", err);
-                return;
+        }).then(udocs => {
+            console.log("udocs.length", udocs.length);
+            for (udoc of udocs) {
+                mothersCounts[udoc._id.organizationId] = udoc.totalMothers;
             }
-            console.log("tdocs.length", tdocs.length);
-            for (tdoc of tdocs) {
-                testsCounts[tdoc._id.organizationId] = tdoc.totalTests;
+            for (var cnt = 0; cnt < documents.length; cnt++) {
+                //allDocuments.push(documents[cnt]);
+                var doc = allDocuments[cnt];
+                //console.log(doc.documentId);
+                if (allDocuments[cnt]["noOfMother"] != mothersCounts[doc.documentId]) {
+                    aggregations.aggregateMothers(doc.documentId);
+                }
+                if (allDocuments[cnt]["noOfTests"] != testsCounts[doc.documentId]) {
+                    aggregations.aggregateTests(doc.documentId);
+                }
+                allDocuments[cnt]["noOfMother"] = mothersCounts[doc.documentId];
+                allDocuments[cnt]["noOfTests"] = testsCounts[doc.documentId];
             }
-            console.log("mothersAggrQuery", mothersAggrQuery);
-            Users.aggregate(mothersAggrQuery, {
-                maxTimeMS: 240000
-            }).then(udocs => {
-                console.log("udocs.length", udocs.length);
-                for (udoc of udocs) {
-                    mothersCounts[udoc._id.organizationId] = udoc.totalMothers;
-                }
-                for (var cnt = 0; cnt < documents.length; cnt++) {
-                    //allDocuments.push(documents[cnt]);
-                    var doc = allDocuments[cnt];
-                    //console.log(doc.documentId);
-                    if (allDocuments[cnt]["noOfMother"] != mothersCounts[doc.documentId]) {
-                        aggregations.aggregateMothers(doc.documentId);
-                    }
-                    if (allDocuments[cnt]["noOfTests"] != testsCounts[doc.documentId]) {
-                        aggregations.aggregateTests(doc.documentId);
-                    }
-                    allDocuments[cnt]["noOfMother"] = mothersCounts[doc.documentId];
-                    allDocuments[cnt]["noOfTests"] = testsCounts[doc.documentId];
-                }
-                //console.log(allDocuments[0], allDocuments[0].costructor);
-                res.status(200).json({
-                    message: "Organizations fetched successfully!",
-                    data: allDocuments,
-                    count: allDocuments.length
-                });
-            }).catch(error => {
-                console.log("Error", error);
-                res.status(500).json({
-                    message: "Fetching Organizations failed!"
-                });
+            //console.log(allDocuments[0], allDocuments[0].costructor);
+            res.status(200).json({
+                message: "Organizations fetched successfully!",
+                data: allDocuments,
+                count: allDocuments.length
             });
+        }).catch(error => {
+            console.log("Error", error);
+            res.status(500).json({
+                message: "Fetching Organizations failed!"
+            });
+        });
 
-        })
+        //})
         // .catch(error => {
         //     console.log("Error", error);
         //     res.status(500).json({
