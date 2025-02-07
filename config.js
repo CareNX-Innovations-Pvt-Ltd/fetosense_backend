4f84d3fab29a9b5c77a9c355d2b33c7a99ae075e
root <root@ip-172-31-29-115.ec2.internal>
2025-02-07 05:01:43 +0000

    awsv2_changes

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 configValue = {
 	"fetosense-v2": {
-		mongoUrl: 'mongodb+srv://dba:Kul-adi_0070@prod-carenx-database.global.mongocluster.cosmos.azure.com/fetosense-v2?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000',
+		mongoUrl: 'mongodb://fetosense:C-nx#25*@fetosense-db-cluster.cluster-cziak2w6e5md.us-east-1.docdb.amazonaws.com:27017/?directConnection=true&retryWrites=false&tls=true&tlsCAFile=global-bundle.pem',
 		old_mongoUrl: 'mongodb://dba:^addy#007#@c.fetosense.mongocluster.cosmos.azure.com:10260/fetosense-v2?ssl=true',
 		mongodbName: 'fetosense-v2',
 		apiUrl: "https://backend.carenx.com:3006/api",
@@ -8,7 +8,7 @@ configValue = {
 		SSLPrivateKey: '/etc/letsencrypt/live/backend.carenx.com/privkey.pem',
 		SSLCertificate: '/etc/letsencrypt/live/backend.carenx.com/fullchain.pem',
 		production: true,
-		SSLHost: true,
+		SSLHost: false,
 		projectId: "fetosense-v2",
 		firebaseApiUrl: "https://us-central1-fetosense-v2.cloudfunctions.net/",
 		port: "3006",
