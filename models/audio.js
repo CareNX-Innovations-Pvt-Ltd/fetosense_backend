ec24b40705103882087de974c0a81ace5c6fc6d2
MansiSableCarenx <mansi@carenx.com>
2025-02-25 16:43:46 +0530

    feat: audio model modified

--- models/audio.js
+++ models/audio.js
@@ -1,6 +1,18 @@
 /*jshint esversion: 6 */
 const mongoose = require("mongoose");
 
+/**
+ * @module models/audio
+ * @description Mongoose schema for the Audio collection.
+ */
+
+/**
+ * Represents an Audio document.
+ * @typedef {Object} Audio
+ * @property {string} _id - Unique identifier for the audio document.
+ * @property {string} id - Additional identifier for the audio document.
+ */
+
 const audioSchema = mongoose.Schema({
   _id: String,
   id: String
@@ -10,5 +22,12 @@ const audioSchema = mongoose.Schema({
     strict: false,
     maxTimeMS: 60000
   });
+
+/**
+* Mongoose model for the Audio collection.
+* @type {mongoose.Model<Audio>}
+*/
+
+/** Export the model */
 
 module.exports = mongoose.model("audio", audioSchema);
