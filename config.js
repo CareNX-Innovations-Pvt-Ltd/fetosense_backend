8719687d51353a2bcd2129c8f5760163fd7512f5
MansiSableCarenx <mansi@carenx.com>
2025-02-25 16:52:20 +0530

    fix: transferred credentials from config.js and babybeat.js to .env

--- config.js
+++ config.js
@@ -1,19 +1,21 @@
+require('dotenv').config();
+
 configValue = {
 	"fetosense-v2": {
-		mongoUrl: 'mongodb://fetosense:C-nx#25*@fetosense-db-cluster.cluster-cziak2w6e5md.us-east-1.docdb.amazonaws.com:27017/fetosense-v2?directConnection=true&retryWrites=false&tls=true&tlsCAFile=global-bundle.pem',
-		old_mongoUrl: 'mongodb://dba:^addy#007#@c.fetosense.mongocluster.cosmos.azure.com:10260/fetosense-v2?ssl=true',
-		mongodbName: 'fetosense-v2',
-		apiUrl: "https://backend.carenx.com:3006/api",
-		apiUrlLocal: "http://localhost:3006/api",
-		SSLPrivateKey: '/etc/letsencrypt/live/backend.carenx.com/privkey.pem',
-		SSLCertificate: '/etc/letsencrypt/live/backend.carenx.com/fullchain.pem',
-		production: true,
-		SSLHost: true,
-		projectId: "fetosense-v2",
-		firebaseApiUrl: "https://us-central1-fetosense-v2.cloudfunctions.net/",
-		port: "3006",
-		logging: true
+		mongoUrl: process.env.MONGO_URL,
+		old_mongoUrl: process.env.OLD_MONGO_URL,
+		mongodbName: process.env.MONGODB_NAME,
+		apiUrl: process.env.API_URL,
+		apiUrlLocal: process.env.API_URL_LOCAL,
+		SSLPrivateKey: process.env.SSL_PRIVATE_KEY,
+		SSLCertificate: process.env.SSL_CERTIFICATE,
+		production: process.env.PRODUCTION === "true",
+		SSLHost: process.env.SSL_HOST === "true",
+		projectId: process.env.PROJECT_ID,
+		firebaseApiUrl: process.env.FIREBASE_API_URL,
+		port: process.env.PORT,
+		logging: process.env.LOGGING === "true"
 	}
 };
-exports.configValue = configValue['fetosense-v2'];
 
+exports.configValue = configValue['fetosense-v2'];
--- models/babybeat.js
+++ models/babybeat.js
@@ -1,36 +1,30 @@
-/*jshint esversion: 6 */
+require('dotenv').config();
 const mongoose = require("mongoose");
 
 var caremotherPublic = {
-  mongoUrl: 'mongodb://fetosense:C-nx#25*@fetosense-db-cluster.cluster-cziak2w6e5md.us-east-1.docdb.amazonaws.com:27017/carenx-public?directConnection=true&retryWrites=false&tls=true&tlsCAFile=../../global-bundle.pem',
-  old_mongoUrl: 'mongodb://dba:^addy#007#@c.fetosense.mongocluster.cosmos.azure.com:10260/carenx-public?ssl=true',
-  mongodbName: 'caremother-public',
-
-  // API Hosted Url
-  apiUrl: "https://www.caremother.co:3004/api",
-  apiUrlLocal: "http://localhost:3004/api",
-  // SSL configuration url for service Hosting on ssl
-  SSLPrivateKey: '/etc/letsencrypt/live/caremother.co/privkey.pem',
-  SSLCertificate: '/etc/letsencrypt/live/caremother.co/fullchain.pem',
-  // Flag to check/ deploy project on live
-  production: true,
-  SSLHost: false,
-  projectId: "caremother-public",
-  firebaseApiUrl: "https://us-central1-caremother-public.cloudfunctions.net/",
-  port: "3004",
-  logging: true
-}
+  mongoUrl: process.env.CAREMOTHER_MONGO_URL,
+  old_mongoUrl: process.env.CAREMOTHER_OLD_MONGO_URL,
+  mongodbName: process.env.CAREMOTHER_MONGODB_NAME,
+  apiUrl: process.env.CAREMOTHER_API_URL,
+  apiUrlLocal: process.env.CAREMOTHER_API_URL_LOCAL,
+  SSLPrivateKey: process.env.CAREMOTHER_SSL_PRIVATE_KEY,
+  SSLCertificate: process.env.CAREMOTHER_SSL_CERTIFICATE,
+  production: process.env.CAREMOTHER_PRODUCTION === "true",
+  SSLHost: process.env.CAREMOTHER_SSL_HOST === "true",
+  projectId: process.env.CAREMOTHER_PROJECT_ID,
+  firebaseApiUrl: process.env.CAREMOTHER_FIREBASE_API_URL,
+  port: process.env.CAREMOTHER_PORT,
+  logging: process.env.CAREMOTHER_LOGGING === "true"
+};
 
 var connBB = mongoose.createConnection(caremotherPublic.mongoUrl, { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true });
-
 
 const usersSchema = mongoose.Schema({
   _id: String,
   documentId: String
-},
-  {
-    collection: 'Users',
-    strict: false
-  });
+}, {
+  collection: 'Users',
+  strict: false
+});
 
 exports.BBUsers = connBB.model("BBUsers", usersSchema);
